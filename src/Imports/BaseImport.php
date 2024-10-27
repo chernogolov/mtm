a445afde8acc486fa88f131d8004bb6628842f20
chernogolov <chernogolovka@list.ru>
2024-10-27 12:34:13 +0500

    Update BaseImport.php

--- src/Imports/BaseImport.php
+++ src/Imports/BaseImport.php
@@ -2,42 +2,73 @@
 
 namespace Chernogolov\Mtm\Imports;
 
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Schema;
+use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+//use Maatwebsite\Excel\Concerns\WithBatchInserts;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class BaseImport implements ToModel, WithHeadingRow
+class BaseImport implements ToCollection, WithHeadingRow, WithCalculatedFormulas, WithChunkReading
 {
     protected $prefix;
     protected $resource;
     protected $data;
+    protected $i = 0;
+    protected $unique_fields = 'id';
+    public $result;
+
 
     public function __construct($prefix, $resource, $data)
     {
         $this->prefix = $prefix;
         $this->resource = $resource;
         $this->data = $data;
+        if(isset($this->resource->unique_fields) && $this->resource->unique_fields)
+            $this->unique_fields = $this->resource->unique_fields;
     }
 
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
+
         $model = 'App\Models\\' . $this->resource->model_name;
-        if(isset($row['id']))
+        $m = new $model();
+        $tableName = $m->getTable();
+        $columns = Schema::getColumnListing($tableName);
+        $i = 0;
+
+        foreach ($rows as $row)
         {
-            $m = $model::where('id', $row['id'])->first();
-            if($m)
+            foreach ($columns as $column_name)
             {
-                foreach ($m->toArray() as $key => $value)
-                    if(isset($row[$key]))
-                        $m->$key = $row[$key];
+                if(isset($row[$column_name]))
+                {
+                    $this->result[$i][$column_name] = $row[$column_name];
+                }
             }
-            else
-            {
-//            $m = new $model([
-//
-//            ]);
-            }
+
+
+            $i++;
         }
 
-        return $m;
+        foreach ($this->result as $item)
+        {
+            $result = $m::updateOrCreate(
+                $item,
+                [$this->unique_fields => $item[$this->unique_fields]]
+            );
+        }
+    }
+
+
+//    public function batchSize(): int
+//    {
+//        return 1000;
+//    }
+
+    public function chunkSize(): int
+    {
+        return 500;
     }
 }
